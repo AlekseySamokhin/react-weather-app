8cbf354273205c695d9a940e9454a2ad351d791c
AlekseySamokhin <lel.dude01@gmail.com>
2022-09-29 21:12:29 +0300

    fix: delete api key

--- src/const.js
+++ src/const.js
@@ -1,5 +1,5 @@
 export const API_END_POINT = "https://api.openweathermap.org/data/2.5/";
-export const API_KEY = "abef88b62ed4dc7a1010153499c6cecf";
+export const API_KEY = "";
 
 export const WeatherDay = {
     tomorrow: "tomorrow",
